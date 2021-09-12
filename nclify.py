6a52cc448657cae4c0a11f35a58ef25f94c1d5bc
Vadim Kaushan <admin@disasm.info>
2021-09-12 09:14:40 +0300

    Fix function name

--- nclify.py
+++ nclify.py
@@ -3,11 +3,8 @@
 import re
 import sys
 
-fi = open(sys.argv[1], "rt")
-fo = open(sys.argv[2], "wt")
 
-
-def new_signal(old_signal, kind):
+def rename_signal(old_signal, kind):
     m = re.search(r'(.*)(\[\d+\])', old_signal)
     if m:
         return m.group(1) + "_" + kind + m.group(2)
@@ -15,14 +12,17 @@ def new_signal(old_signal, kind):
         return old_signal + "_" + kind
 
 
+fi = open(sys.argv[1], "rt")
+fo = open(sys.argv[2], "wt")
+
 for line in fi:
     if line.startswith(".inputs ") or line.startswith(".outputs "):
         a = line.split()
         names = a[1:]
         new_names = []
         for name in names:
-            new_names.append(new_signal(name, "t"))
-            new_names.append(new_signal(name, "f"))
+            new_names.append(rename_signal(name, "t"))
+            new_names.append(rename_signal(name, "f"))
         fo.write(" ".join([a[0]] + new_names) + "\n")
     elif line.startswith(".subckt"):
         a = line.split()
@@ -31,8 +31,8 @@ for line in fi:
         for arg in a[2:]:
             parts = arg.split("=")
             assert len(parts) == 2
-            new_args.append(parts[0] + "_t=" + new_signal(parts[1], "t"))
-            new_args.append(parts[0] + "_f=" + new_signal(parts[1], "f"))
+            new_args.append(parts[0] + "_t=" + rename_signal(parts[1], "t"))
+            new_args.append(parts[0] + "_f=" + rename_signal(parts[1], "f"))
         fo.write(" ".join([a[0], new_type] + new_args) + "\n")
     else:
         fo.write(line)
